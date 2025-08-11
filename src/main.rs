c48331f0312023ac6f4e5ed2e03edb4b411268c4
Just_Anniee <145839789+Annieeeee11@users.noreply.github.com>
2025-08-11 08:22:41 +0530

    .

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,10 @@
-/* this is a guess number game and the concepts that i learned from this is are following:
-1. use of rand crate to generate random numbers
-2. use of io crate to read user input
-3. use of loop
-4. use of if else to check the user input
-5. Ownership & borrowing (mutable + immutable)
-6. Copy vs move
-7. Functions taking & returning values
-8. Loops & conditionals
-9. User input
-*/
-
-use std::io;
 use rand::Rng;
-
-fn get_guess() -> u32 {
-    let mut user_input = String::new();
-    io::stdin().read_line(&mut user_input).expect("Failed to read line");
-    let users_input: u32 = user_input.trim().parse().expect("Please enter a number");
-    users_input
-  }
-  
-  fn check_guess(secret_num: u32) {
-    for attempt in 1..=10 {
-        let user_guess = get_guess();
-
-        if user_guess == secret_num {
-            println!("You got it!");
-            return;
-        } else if user_guess > secret_num {
-            println!("Too high!");
-        } else {
-            println!("Too low!");
-        }
-
-        if attempt < 10 {
-            println!("Please guess again.");
-        }
-    }
-    println!("You lost! The number was {}.", secret_num);
-  }
+mod part2;
+use part2::{get_guess, check_guess, User};
   
   fn main() {
+
+    //guess number game 
     println!("You have 10 guesses to guess the number.");
     println!("how can rounds do you want to play?");
     let rounds = get_guess();
@@ -49,7 +13,13 @@ fn get_guess() -> u32 {
         println!("Round {}: Guess the number!", i + 1);
         check_guess(secret_num);
     }
-    // let secret_num = "15";
-    // let secret_num_2: String = Default::default();
-    // println!("{},{},{},{:p}", secret_num_2.capacity(), secret_num.len(), secret_num.is_empty(), secret_num.as_ptr());  
+
+    //Structs
+    let user1 = User {
+        name: String::from("John"),
+        id: 1,
+        email: String::from("john@example.com"),
+        age: 20,
+    };
+    println!("{},{},{},{}",user1.name,user1.id,user1.email,user1.age ) 
   }
