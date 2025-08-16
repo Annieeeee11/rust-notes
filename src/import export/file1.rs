3135778d52e5943e76fa6826c244b17fafe63c29
Just_Anniee <145839789+Annieeeee11@users.noreply.github.com>
2025-08-16 17:20:25 +0530

    .

---
+++ src/import export/file1.rs
@@ -0,0 +1,16 @@
+/* import a file which is inside a folder */
+mod folder1;
+use folder1::example::testfunction12;
+/* simple import */
+mod file2;
+use file2::testfunction1;
+
+fn main() {
+    let a = 5;
+    let b = 10;
+
+    let res1 = testfunction1(a,b); 
+    let res2 = testfunction12(a,b);
+
+    println!("{} , {}", res1,res2);
+}
---
+++ src/import export/folder1/example.rs
@@ -0,0 +1,3 @@
+pub fn testfunction12(a: u32, b:u32) -> u32 {
+    a + b
+}
