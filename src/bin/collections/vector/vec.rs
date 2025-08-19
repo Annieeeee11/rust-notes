9ec81ceea33b0e52240719f168bf324ef0b96aab
Just_Anniee <145839789+Annieeeee11@users.noreply.github.com>
2025-08-19 22:26:25 +0530

    .

---
+++ src/bin/collections/vector/vec.rs
@@ -0,0 +1,41 @@
+fn main() {
+    let mut x = Vec::new(); // new vector
+    x.push(1); //pushed values cause it is mutable
+    x.push(2);
+    x.push(3);
+    println!("{:?}", x); //print vector 
+
+    let u = evenn(&x);
+    println!("{:?}", u);
+    let m = vec(&mut x);
+    println!("{:?}", m);
+    let y = remove(&mut x);
+    println!("{:?}", y);
+}
+
+fn vec(x: &mut Vec<u32>) -> Vec<u32> {
+    let mut i = 0;
+    while i < x.len() {
+        if x[i] % 2 != 0 {
+            x.remove(i);
+        } else {
+            i += 1;
+        }
+    }
+    x.to_vec()
+}
+
+fn evenn(x: &Vec<u32>) -> Vec<u32> {
+    let mut new_vec = Vec::new();
+    for val in x {
+        if val % 2 == 0 {
+            new_vec.push(*val);
+        }
+    }
+    return new_vec;
+}
+
+fn remove(a: &mut Vec<u32>) -> Vec<u32> {
+    a.retain(|&i| i % 2 != 0); //retains return nothing its return type os () to we need to make a copy of vect to return a
+    a.to_vec()
+}
