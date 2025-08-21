a4296ec3170d36dcb92a6af7030dfa1ca91cc40d
Just_Anniee <145839789+Annieeeee11@users.noreply.github.com>
2025-08-21 17:07:24 +0530

    .

---
+++ src/practice programs/book_manager/src/bin/main.rs
@@ -0,0 +1,158 @@
+use std::io;
+
+// book structure
+struct Book {
+    title: String,
+    author: String,
+    price: u32,
+    id: u32,
+}
+
+//command enum
+enum Commands {
+    Add,
+    Search,
+    List,
+    Remove,
+    Exit,
+}
+
+//return the commands to avoid any errors
+impl Commands {
+    fn from_input(input: &str) -> Option<Commands> {
+        match input.trim().to_lowercase().as_str() {
+            "add" => Some(Commands::Add),
+            "list" => Some(Commands::List),
+            "search" => Some(Commands::Search),
+            "remove" => Some(Commands::Remove),
+            "exit" => Some(Commands::Exit),
+            _ => None,
+        }
+    }
+}
+
+// input function
+fn user_input() -> String {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input.trim().to_string()
+}
+
+// library that takes book structure
+struct Library {
+    book: Vec<Book>,
+}
+
+// library function
+impl Library {
+
+// normal constructor 
+    /* fn new() -> Self {
+        Self { books: Vec::new() }
+    } */
+
+
+// default trait 
+    fn default() -> Self {
+        Library { book: Vec::new() }
+    }
+
+
+// add
+    fn add_book(&mut self) -> Result<(), String> {
+        println!("enter book title: ");
+        let title = user_input();
+        println!("enter author: ");
+        let author = user_input();
+        println!("enter price: ");
+        let price: u32 = user_input()
+            .parse()
+            .map_err(|_| "Invalid price".to_string())?;
+        let id = (self.book.len() + 1) as u32;
+        self.book.push(Book { id, title, author, price });
+        println!("Book added successfully!");
+        Ok(())
+    }
+
+
+// search
+    fn search_book(&self) {
+        println!("enter book title: ");
+        let search_input = user_input().to_lowercase();
+        let result = self.book.iter().find(|b| b.title.to_lowercase() == search_input);
+
+        match result {
+            Some(b) => println!("book -> ID: {}, Title: {}, Author: {}, Price: {}", b.id, b.title, b.author, b.price),
+            None => println!("book not found"),
+        }
+    }
+
+
+// list
+    fn list_books(&self) {
+        if self.book.is_empty() {
+            println!("no books in library.");
+        } else {
+            for b in &self.book {
+                println!("ID: {}, Title: {}, Author: {}, Price: {}", b.id, b.title, b.author, b.price);
+            }
+        }
+    }
+
+
+// remove
+    fn remove_book(&mut self) -> Result<(), String> {
+        println!("Enter book ID to remove:");
+        let id: u32 = user_input()
+        .parse()
+        .map_err(|_| "Invalid ID".to_string())?;
+
+        if let Some(pos) = self.book.iter().position(|b| b.id == id) {
+            self.book.remove(pos);
+            println!("Book removed successfully!");
+            Ok(())
+        } else {
+            Err("Book ID not found".to_string())
+        }
+    }
+}
+
+fn main() {
+/* this can be like a way to make a object */
+    // let my_library = Library { books: Vec::new() };
+
+
+/* using constructor (new function will be made in impl library) */
+    // let library = Library::new();
+
+
+/* using deafult trait in rust */
+    let mut library = Library::default();
+
+    loop {
+        println!("Enter command => add, search, list, remove, exit");
+        let command = user_input();
+
+        match Commands::from_input(&command) {
+            Some(Commands::Add) => {
+                if let Err(e) = library.add_book() {
+                    println!("Error: {}", e);
+                }
+            }
+            Some(Commands::List) => library.list_books(),
+            Some(Commands::Search) => library.search_book(),
+            Some(Commands::Remove) => {
+                if let Err(e) = library.remove_book() {
+                    println!("Error: {}", e);
+                }
+            }
+            Some(Commands::Exit) => {
+                println!("Goodbye!");
+                break;
+            }
+            None => println!("Invalid command, try again."),
+        }
+    }
+}
---
+++ src/practice programs/book_manager/src/main.rs
@@ -0,0 +1,72 @@
+use std::io;
+
+struct Book{  // book structure
+    title: String,
+    author: String,
+    price: u32,
+    id: u32,
+}
+
+// enum Commands { //command enum
+//     Add,
+//     Search,
+//     List,
+//     Remove,
+//     Exit,
+// }
+
+fn user_input() -> String { // input function
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    input.trim().to_string()
+}
+
+fn add_book(library: &mut Vec<Book>) {
+    println!("Enter book title: ");
+    let title = user_input();
+    println!("Enter author: ");
+    let author = user_input();
+    println!("Enter price: ");
+    let price: u32 = user_input().parse().unwrap_or(0);
+    let id = library.len() as u32 + 1; // simple auto ID
+    let book = Book { title, author, price, id };
+    library.push(book);
+    println!("Book added!");
+}
+
+fn list_books(library: &Vec<Book>) {
+    for book in library {
+        println!("ID: {}, Title: {}, Author: {}, Price: {}", book.id, book.title, book.author, book.price);
+    }
+}
+
+fn search_book(library: &Vec<Book>) {
+    println!("Enter book title to search:");
+    let search_input = user_input();
+    let search_input = search_input.to_lowercase();
+    for book in library {
+        if book.title.to_lowercase() == search_input {
+            println!("ID: {}, Title: {}, Author: {}, Price: {}", book.id, book.title, book.author, book.price);
+        }
+    }
+}
+
+// fn _remove_book(library: &mut Vec<Book>) {
+// }
+
+fn main() {
+    let mut library: Vec<Book> = Vec::new(); // We’ll store multiple books here
+
+    loop {
+        println!("Enter command => add, Search, List, Remove, Exit");
+        let command = user_input(); 
+        match command.to_lowercase().as_str() {
+            "add" => add_book(&mut library),
+            "search" => search_book(&library),
+            "list" => list_books(&library),
+            // "remove" => remove_book(&mut library),
+            "exit" => break,
+            _ => println!("Unknown command"),
+        }
+    }
+}
